9d59a699186aef9ea863e7d9447fbe69ee7a5eb3
Esmeralda2001 <43240800+Esmeralda2001@users.noreply.github.com>
2019-11-27 21:17:13 +0100

    GuiObject: only set up properties

--- org/graphics/GuiObject.java
+++ org/graphics/GuiObject.java
@@ -2,10 +2,42 @@ package org.graphics;
 
 public class GuiObject {
 
-    public boolean Active;
-    public Vector2 AnchorPoint;
     //public Color3 BackgroundColor; 
+    //public Color3 BorderColor3;
+    //public BorderMode BorderMode;
+    //public SizeConstraint SizeConstraint;
+
+    // BOOL VALUES
+    public boolean Active;
+    public boolean ClipsDescendants; 
+    public boolean Selectable; 
+    public boolean Visible;
+
+    //VECTOR VALUES
+    public Vector2 AnchorPoint;
+
+
+    public int BorderSizePixel;
+    public int LayOutOrder;
+    public int ZIndex;
+
+    public GuiObject NextSelectionDown; 
+    public GuiObject NextSelectionLeft; 
+    public GuiObject NextSelectionRight; 
+    public GuiObject NextSelectionUp; 
+
+    public UDim2 Position; 
+    public UDim2 Size;
+
+    public float Rotation;
+    public float Transparency;
     public float BackGroundTransparency; 
+
+    public GuiObject SelectionImageObject;
     
+  
+
+    //INHERITANCE: GuiBase2d, Instance
+
 
 }
